670248f43b1728e531c5baa4256206f9686be43a
iamhrk <iamhrk8@gmail.com>
2023-09-08 16:31:21 +0530

    First commit

---
+++ 1. Two Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i<nums.length-1; i++){
+            int current_num = nums[i];
+            for(int j = i+1; j<nums.length;j++){
+                int next_num = nums[j];
+                if(next_num + current_num == target){
+                    return new int[]{i,j};
+                } 
+            }
+        }
+        return new int[]{};
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Daily Competitive Programming
+
+Programming Questions that I try to solve everyday to improve coding skills.
+
+
